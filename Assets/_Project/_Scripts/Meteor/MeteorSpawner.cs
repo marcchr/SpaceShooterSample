5de80a9ad46e47bd81a95972d9336ec5eb6195cf
Jolo Gutierrez <mgutierrez@htechcorp.net>
2024-09-09 08:14:37 +0800

    Set meteor spawner to be a random interval spawner

--- Assets/_Project/_Scripts/Meteor/MeteorSpawner.cs
+++ Assets/_Project/_Scripts/Meteor/MeteorSpawner.cs
@@ -1,20 +1,29 @@
 using UnityEngine;
 
-public class MeteorSpawner : MonoBehaviour, IIntervalSpawner
+public class MeteorSpawner : MonoBehaviour, IRandomIntervalSpawner
 {
     [SerializeField] private Transform[] _spawnPoints;
     public Transform[] SpawnPoints => _spawnPoints;
 
-    [SerializeField] private float _spawnInterval;
+    [SerializeField] private float _minSpawnInterval = 3f, _maxSpawnInterval = 5f;
+    private float _spawnInterval;
     public float Interval => _spawnInterval;
+    public float MinimumInterval => _minSpawnInterval;
+    public float MaximumInterval => _maxSpawnInterval;
 
     [SerializeField] private GameObject _meteorPrefab;
     public GameObject SpawnObjectPrefab => _meteorPrefab;
+
 
     [SerializeField] private float _spawnRadius;
     [SerializeField] private Transform _planetTransform;
 
     private float _elapsedTime;
+
+    private void Start()
+    {
+        _spawnInterval = Random.Range(_minSpawnInterval, _maxSpawnInterval);
+    }
 
     private void Update()
     {
@@ -28,6 +37,7 @@ public class MeteorSpawner : MonoBehaviour, IIntervalSpawner
             var meteor = Instantiate(_meteorPrefab, spawnPoint.position + (Vector3)Random.insideUnitCircle * _spawnRadius, Quaternion.identity).GetComponent<Meteor>();
             meteor.Move(_planetTransform.position - meteor.transform.position);
             _elapsedTime = 0f;
+            _spawnInterval = Random.Range(_minSpawnInterval, _maxSpawnInterval);
         }
     }
 }
